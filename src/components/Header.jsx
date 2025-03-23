5d09a577e08f225c8e23e6abdb0dd50c0bca1d65
Ravikirannnnn <ravikiranrcta@gmail.com>
2025-03-23 17:50:42 +0530

    us

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.css";
 import "./Header.css"
 import weddingBg from "../assets/fahmi-ramadhan-WvFKZKzOTEM-unsplash 1.png";
 import leaf from "../assets/leaf.png"
-import { useState } from "react";
+import { useState ,useEffect} from "react";
 
 const Header = () => {
   const [bgImage, setBgImage] = useState(null); // Start with placeholder
